0ee2ccd3315c9a07849c7dc7ed689fdada5a1dd6
Hung Nguyen Viet <hungnguyenviet.hatinh@gmail.com>
2020-07-14 20:59:01 +0700

    Updated logo.

--- src/components/Layout/SideBar/SideBar.js
+++ src/components/Layout/SideBar/SideBar.js
@@ -7,7 +7,7 @@ import FacebookLink from '../../FacebookLink';
 import Link from '../../Link';
 import LinkedinLink from '../../LinkedinLink';
 
-import logo from '../Header/logo_001.png';
+import logo from '../../../static/images/logo_001.svg';
 import './SideBar.scss';
 
 const SideBar = (props) => {
