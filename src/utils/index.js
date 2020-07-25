a741f0b7771f692857977846529a85c1db3507af
hungnguyenviethatinh <hungnguyenviet.hatinh@gmail.com>
2020-07-25 16:35:25 +0700

    update encodeURI for API get blogs

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,7 @@
-export { parseTotalFromContentRange } from './Utils';
+export { 
+    parseTotalFromContentRange,
+    createRangePaginationQuery 
+} from './Utils';
 export { LANDING_PAGE_AIMS, ABOUT_US } from './AppData';
 export { formatDate } from './DateTimeFormat';
 export { default as browserHistory } from './BrowserHistory';
